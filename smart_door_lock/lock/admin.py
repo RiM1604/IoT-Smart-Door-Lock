7bc4521681a3e51a05a5892b6eed1d6312bb826b
Ritesah Madhunala <ritesah-ubuntu@ritesah-madhunala-ubuntu-22.04-LTS>
2024-11-12 23:51:26 +0530

    Created the frontend but need to check with the hardware

--- smart_door_lock/lock/admin.py
+++ smart_door_lock/lock/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import AdminSettings
+from .models import AdminSettings, AccessLog
 # Register your models here.
 
 
 admin.site.register(AdminSettings)
+admin.site.register(AccessLog)
--- smart_door_lock/smart_door_lock/settings.py
+++ smart_door_lock/smart_door_lock/settings.py
@@ -114,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Kolkata"
 
-USE_I18N = True
+# USE_I18N = True
 
 USE_TZ = True
 
